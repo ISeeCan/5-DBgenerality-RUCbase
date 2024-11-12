ec28cfb0067e98e85081bba14ccaf8ed977bdb68
ISeeCan-U-BL <w1098681119@163.com>
2024-11-12 14:45:28 +0800

    Lab1 is done, let's test correctness

--- README.md
+++ README.md
@@ -10,11 +10,11 @@
 
 ##### 任务1.1 磁盘存储管理器：
 
-    补全src/storage/disk_manager.cpp中的函数
+补全src/storage/disk_manager.cpp中的函数
 
-    依赖的函数如下，需要手动补全的有前缀-：
+依赖的函数如下，需要手动补全的有前缀-：
 
-    ``` cpp
+``` cpp
     - void write_page(int fd, page_id_t page_no, const char *offset, int num_bytes);
     - void read_page(int fd, page_id_t page_no, char *offset, int num_bytes);
     page_id_t allocate_page(int fd);
@@ -23,49 +23,51 @@
     - int open_file(const std::string &path);
     - void close_file(int fd);
     - void destroy_file(const std::string &path);
-    ```
+```
 
-    相关知识及系统函数：
-    ``` plaintext
-    lseek(fd, offset, whence)
-    lseek() 是一个文件操作系统调用，用于在文件中移动文件指针。它的三个参数是：
+相关知识及系统函数：
 
-    fd：文件描述符。
-    offset：偏移量，以字节为单位。这里传入的是 page_no * PAGE_SIZE，表示目标页在文件中的位置。通过这个计算，我们可以跳转到特定页。
-    whence：参照位置，SEEK_SET 表示从文件开头开始偏移 offset 个字节，直接定位到指定页的位置。
-    假设 PAGE_SIZE 为 4096 字节（典型数据库页大小），如果 page_no = 2，lseek 将把文件指针定位到 2 * 4096 = 8192 字节处，相当于文件的第三个页位置。
+lseek(fd, offset, whence)
+lseek() 是一个文件操作系统调用，用于在文件中移动文件指针。它的三个参数是：
 
-    lseek 的返回值是新文件指针的位置（即新的偏移量），如果返回值是 -1，说明定位失败，通常会抛出 UnixError。
-    ```
+fd：文件描述符。
+
+offset：偏移量，以字节为单位。这里传入的是 page_no * PAGE_SIZE，表示目标页在文件中的位置。通过这个计算，我们可以跳转到特定页。
+
+whence：参照位置，SEEK_SET 表示从文件开头开始偏移 offset 个字节，直接定位到指定页的位置。假设 PAGE_SIZE 为 4096 字节（典型数据库页大小），如果 page_no = 2，lseek 将把文件指针定位到 2 * 4096 = 8192 字节处，相当于文件的第三个页位置。
+
+lseek 的返回值是新文件指针的位置（即新的偏移量），如果返回值是 -1，说明定位失败，通常会抛出 UnixError。
+    
 
 ##### 任务1.2 缓冲池替换策略
 
-    补全`Replacer`类，其负责跟踪缓冲池中每个页面所在帧的使用情况。当缓冲池没有空闲页面时，需要使用该类提供的替换策略选择一个页面进行淘汰。要求实现的替换策略为最近最少使用（LRU）算法。
+补全`Replacer`类，其负责跟踪缓冲池中每个页面所在帧的使用情况。当缓冲池没有空闲页面时，需要使用该类提供的替换策略选择一个页面进行淘汰。要求实现的替换策略为最近最少使用（LRU）算法。
 
-    需要补全的文件位于：src/replacer/lru_replacer.cpp
+需要补全的文件位于：src/replacer/lru_replacer.cpp
 
-    ``` cpp
+``` cpp
     bool LRUReplacer::victim(frame_id_t* frame_id)
     void LRUReplacer::pin(frame_id_t frame_id) 
     void LRUReplacer::unpin(frame_id_t frame_id)
-    ```
-    知识点
+```
 
-    ``` plaintext
-    知识点：std::scoped_lock
-    功能：std::scoped_lock 是 C++17 引入的一种锁，能够实现作用域内自动加锁和解锁功能。std::scoped_lock 会在构造时自动上锁，在作用域结束或遇到异常退出时自动解锁，避免因未手动解锁而造成的死锁问题。
-    锁对象 latch_：latch_ 是一个互斥锁（std::mutex），保护函数的关键区域，确保只有一个线程能够进入并执行操作。其他线程只能在加锁结束后才能继续访问。
-    ```
+知识点：std::scoped_lock
+
+功能：std::scoped_lock 是 C++17 引入的一种锁，能够实现作用域内自动加锁和解锁功能。std::scoped_lock 会在构造时自动上锁，在作用域结束或遇到异常退出时自动解锁，避免因未手动解锁而造成的死锁问题。
+    
+锁对象 latch_：latch_ 是一个互斥锁（std::mutex），保护函数的关键区域，确保只有一个线程能够进入并执行操作。其他线程只能在加锁结束后才能继续访问。
 
 ##### 任务1.3 缓冲池管理器
 
-    补全`BufferPoolManager`类，其负责管理缓冲池中的页面与磁盘文件中的页面之间的来回移动。
+补全`BufferPoolManager`类，其负责管理缓冲池中的页面与磁盘文件中的页面之间的来回移动。
 
-    文件位置：src/storage/buffer_pool_manager.cpp
+文件位置：src/storage/buffer_pool_manager.cpp
 
-    需补全函数：
+[链接](src/storage/buffer_pool_manager.cpp)
+
+需补全函数：
     
-    ``` cpp
+``` cpp
     public:
         BufferPoolManager(size_t pool_size, DiskManager *disk_manager);
         ~BufferPoolManager();
@@ -80,3 +82,56 @@
         - bool find_victim_page(frame_id_t *frame_id);
         - void update_page(Page *page, PageId new_page_id, frame_id_t new_frame_id);
     ```
+
+##### 任务2.1 记录操作
+
+补全`RMFileHandle`类，其负责对文件中的记录进行操作。
+
+每个`RMFileHandle`对应一个记录文件，当`RMManager`执行打开文件操作时，便会创建一个指向`RMFileHandle`的指针。文件位于：src/record/rm_file_handle.cpp
+
+[链接](src/record/rm_file_handle.cpp)
+
+`RMFileHandle`类的接口如下：
+
+``` cpp
+    public:
+    - RmFileHandle(DiskManager *disk_manager, BufferPoolManager *buffer_pool_manager, int fd);
+    // 不考虑事务的记录操作（事务将在后续实验使用）
+    - std::unique_ptr<RmRecord> get_record(const Rid &rid, Context *context) const;
+    - Rid insert_record(char *buf, Context *context);
+    - void delete_record(const Rid &rid, Context *context);
+    - void update_record(const Rid &rid, char *buf, Context *context);
+    // 辅助函数
+    - RmPageHandle create_new_page_handle();
+    - RmPageHandle fetch_page_handle(int page_no) const;
+    - RmPageHandle create_page_handle();
+    - void release_page_handle(RmPageHandle &page_handle);
+```
+
+##### 任务2.2 记录迭代器
+
+本任务要求补全`RmScan`类，其用于遍历文件中存放的记录。
+
+文件位置：src/record/rm_scan.cpp
+
+[链接](src/record/rm_scan.cpp)
+
+`RmScan`类继承于`RecScan`类，它们的接口如下：
+
+```cpp
+    class RecScan {
+    public:
+        virtual ~RecScan() = default;
+        virtual void next() = 0;
+        virtual bool is_end() const = 0;
+        virtual Rid rid() const = 0;
+    };
+
+    class RmScan : public RecScan {
+    public:
+        - RmScan(const RmFileHandle *file_handle);
+        - void next() override;
+        - bool is_end() const override;
+        Rid rid() const override;
+    };
+```
--- src/record/rm_file_handle.cpp
+++ src/record/rm_file_handle.cpp
@@ -20,8 +20,9 @@ std::unique_ptr<RmRecord> RmFileHandle::get_record(const Rid& rid, Context* cont
     // Todo:
     // 1. 获取指定记录所在的page handle
     // 2. 初始化一个指向RmRecord的指针（赋值其内部的data和size）
-
-    return nullptr;
+    RmPageHandle pagehandle = fetch_page_handle(rid.page_no);// 通过指定的Rid获取相应页面的句柄
+    std::unique_ptr<RmRecord> recordptr{new RmRecord(file_hdr_.record_size, pagehandle.get_slot(rid.slot_no))};// 创建一个std::unique_ptr<RmRecord>，指向一个RmRecord对象，使用页面句柄的get_slot方法获取记录的指针
+    return recordptr;// 返回记录的unique_ptr指针
 }
 
 /**
@@ -38,7 +39,30 @@ Rid RmFileHandle::insert_record(char* buf, Context* context) {
     // 4. 更新page_handle.page_hdr中的数据结构
     // 注意考虑插入一条记录后页面已满的情况，需要更新file_hdr_.first_free_page_no
 
-    return Rid{-1, -1};
+    RmPageHandle pagehandle = create_page_handle();  // 创建一个页面句柄
+    int i = Bitmap::first_bit(0, pagehandle.bitmap, file_hdr_.num_records_per_page);  // 在页面中找到可用的插槽位置
+
+    if (i == -1) {
+        // 如果当前页面已满，需要查找下一个未满页面来插入记录
+        for (int pageNo = file_hdr_.first_free_page_no; pageNo != -1; pageNo = pagehandle.page_hdr->next_free_page_no) {
+            pagehandle = fetch_page_handle(pageNo);
+            i = Bitmap::first_bit(0, pagehandle.bitmap, file_hdr_.num_records_per_page);
+            if (i != -1) {
+                break;  // 找到了未满的页面，退出循环
+            }
+        }
+    }
+
+    char *slot = pagehandle.get_slot(i);  // 获取该位置的插槽
+    memcpy(slot, buf, file_hdr_.record_size);  // 将数据从 buf 复制到插槽
+    Bitmap::set(pagehandle.bitmap, i);  // 在位图中设置相应的位，表示插槽已被占用
+    pagehandle.page_hdr->num_records++;  // 增加页面中的记录计数
+
+    if (pagehandle.page_hdr->num_records >= file_hdr_.num_records_per_page) {
+        file_hdr_.first_free_page_no = pagehandle.page_hdr->next_free_page_no;
+    }
+
+    return Rid{pagehandle.page->get_page_id().page_no, i};  // 返回表示插入记录位置的 Rid
 }
 
 /**
@@ -47,7 +71,20 @@ Rid RmFileHandle::insert_record(char* buf, Context* context) {
  * @param {char*} buf 要插入记录的数据
  */
 void RmFileHandle::insert_record(const Rid& rid, char* buf) {
-    
+    if (rid.page_no < file_hdr_.num_pages) {
+        create_new_page_handle();
+    }
+    RmPageHandle pageHandle = fetch_page_handle(rid.page_no);
+    Bitmap::set(pageHandle.bitmap, rid.slot_no);
+    pageHandle.page_hdr->num_records++;
+    if (pageHandle.page_hdr->num_records == file_hdr_.num_records_per_page) {
+        file_hdr_.first_free_page_no = pageHandle.page_hdr->next_free_page_no;
+    }
+
+    char *slot = pageHandle.get_slot(rid.slot_no);
+    memcpy(slot, buf, file_hdr_.record_size);
+
+    buffer_pool_manager_->unpin_page(pageHandle.page->get_page_id(), true);
 }
 
 /**
@@ -60,6 +97,13 @@ void RmFileHandle::delete_record(const Rid& rid, Context* context) {
     // 1. 获取指定记录所在的page handle
     // 2. 更新page_handle.page_hdr中的数据结构
     // 注意考虑删除一条记录后页面未满的情况，需要调用release_page_handle()
+    RmPageHandle pagehandle = fetch_page_handle(rid.page_no);// 获取记录所在页面的句柄
+    Bitmap::reset(pagehandle.bitmap,rid.slot_no); // 在位图中重置指定插槽的位，表示插槽为空闲
+    pagehandle.page_hdr->num_records--;// 减少页面中的记录计数
+    if(pagehandle.page_hdr->num_records==file_hdr_.num_records_per_page-1)// 如果页面中的记录数为每页最大记录数减1，说明刚刚是满的，释放页面句柄
+    {
+        release_page_handle(pagehandle);
+    }
 }
 
 
@@ -73,7 +117,9 @@ void RmFileHandle::update_record(const Rid& rid, char* buf, Context* context) {
     // Todo:
     // 1. 获取指定记录所在的page handle
     // 2. 更新记录
-
+    RmPageHandle pagehandle = fetch_page_handle(rid.page_no);// 获取包含指定记录的页面句柄
+    char *slot = pagehandle.get_slot(rid.slot_no);// 获取指定插槽的地址
+    memcpy(slot, buf, file_hdr_.record_size);// 将新记录数据从buf复制到插槽
 }
 
 /**
@@ -88,7 +134,15 @@ RmPageHandle RmFileHandle::fetch_page_handle(int page_no) const {
     // Todo:
     // 使用缓冲池获取指定页面，并生成page_handle返回给上层
     // if page_no is invalid, throw PageNotExistError exception
-
+    if (page_no == INVALID_PAGE_ID)// 创建一个新的PageId，用于表示新页面
+    {
+        throw PageNotExistError("table name?", page_no);
+    } 
+    else// 从缓冲池管理器中获取指定页面的句柄
+    {
+        Page *page = buffer_pool_manager_->fetch_page(PageId{this->fd_, page_no});
+        return RmPageHandle(&file_hdr_, page);// 返回一个RmPageHandle对象，将文件头和页面传递给它
+    }
     return RmPageHandle(&file_hdr_, nullptr);
 }
 
@@ -101,8 +155,15 @@ RmPageHandle RmFileHandle::create_new_page_handle() {
     // 1.使用缓冲池来创建一个新page
     // 2.更新page handle中的相关信息
     // 3.更新file_hdr_
-
-    return RmPageHandle(&file_hdr_, nullptr);
+    PageId *newpageid = new PageId;
+    newpageid->fd = fd_;
+    Page *newpage = buffer_pool_manager_->new_page(newpageid);// 创建一个RmPageHandle对象，将文件头和新页面传递给它
+    RmPageHandle newPageHandle = RmPageHandle(&file_hdr_, newpage);// 创建一个RmPageHandle对象，将文件头和新页面传递给它
+    newPageHandle.page_hdr->next_free_page_no = file_hdr_.first_free_page_no;// 设置新页面的下一个可用页面编号为文件头的first_free_page_no
+    // 更新文件头的first_free_page_no和num_pages字段
+    file_hdr_.first_free_page_no = newpageid->page_no;
+    file_hdr_.num_pages++;
+    return newPageHandle;// 返回新页面句柄
 }
 
 /**
@@ -117,7 +178,14 @@ RmPageHandle RmFileHandle::create_page_handle() {
     //     1.1 没有空闲页：使用缓冲池来创建一个新page；可直接调用create_new_page_handle()
     //     1.2 有空闲页：直接获取第一个空闲页
     // 2. 生成page handle并返回给上层
-
+    if (file_hdr_.first_free_page_no == -1)// 如果文件头的first_free_page_no为-1，说明没有空闲页面，需要创建新的页面句柄
+    {
+        return create_new_page_handle();
+    }
+    else// 否则，获取指定页面编号的页面句柄，并返回该句柄
+    {
+        return fetch_page_handle(file_hdr_.first_free_page_no);
+    }
     return RmPageHandle(&file_hdr_, nullptr);
 }
 
@@ -129,5 +197,6 @@ void RmFileHandle::release_page_handle(RmPageHandle&page_handle) {
     // 当page从已满变成未满，考虑如何更新：
     // 1. page_handle.page_hdr->next_free_page_no
     // 2. file_hdr_.first_free_page_no
-    
+    page_handle.page_hdr->next_free_page_no = file_hdr_.first_free_page_no;// 设置页面头的下一个可用页面编号为文件头的first_free_page_no
+    file_hdr_.first_free_page_no = page_handle.page->get_page_id().page_no;// 更新文件头的first_free_page_no为页面的页号
 }
